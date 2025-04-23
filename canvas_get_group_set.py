2eb6692f0ab9b93e7b3a160d7dbd83a9b0f1ea6c
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-04-23 11:35:11 -0700

    pc - update canvas_get_group_set.py script to pull in group leaders information

--- canvas_get_group_set.py
+++ canvas_get_group_set.py
@@ -30,34 +30,72 @@ def get_groups(category_id):
         all_groups.extend(data)
         # Check for pagination
         url = response.links.get('next', {}).get('url')
-    pprint(all_groups)  # For debugging/exploration of data
     return all_groups
 
 def get_group_members(group_id):
     url = f"{API_URL}/groups/{group_id}/users"
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
+    # pprint(response.json())  # For debugging/exploration of data
     return response.json()
 
 def export_group_data_to_csv(groups):
     with open("canvas_group_export.csv", "w", newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Group Name", "Name", "Perm", "Email"])
+        writer.writerow(["id", "Group Name", "Name", "Perm", "Email", "LoginId", "Leader Id", "Leader Name", "Leader Email"])
+
+        # First pass; get all groups and their members
+        
+        students = {}
 
         for group in groups:
             group_name = group['name']
             group_id = group['id']
+            if group['leader'] and group['leader']['id']:
+                # If the group has a leader, get their display name
+                leader_id = group['leader']['id']
+            else:
+                # If the group does not have a leader, set leader to None
+                leader_id = None
             users = get_group_members(group_id)
             for user in users:
                 # pprint(user) # for debugging/exploration of data
                 loginId = user.get("login_id")
                 email = f"{loginId}@ucsb.edu"
-                writer.writerow([
-                    group_name,
-                    user.get("name"),
-                    user.get("integration_id"),
-                    email
-                ])
+                students[user.get("id")] = {
+                    'id': user.get("id"),
+                    'group_name': group_name,
+                    'name': user.get("name"),
+                    'integration_id': user.get("integration_id"),
+                    'email': email,
+                    'loginId': loginId,
+                    'leader_id': leader_id,
+                    'leader_name': None, # Placeholder for leader name
+                    'leader_email': None # Placeholder for leader email
+                }
+                
+        # Fill in leader names and emails
+        for student in students.values():
+            try:
+                leader_id = student['leader_id']
+                if leader_id:
+                    # If the leader exists in the students dictionary, get their details
+                    student['leader_name'] = students[leader_id]['name']
+                    student['leader_email'] = students[leader_id]['email']
+            except KeyError:
+                pprint(f"WARNING: Leader ID {leader_id} for student {student} not found in students dictionary.")
+       
+        for student in students.values():
+            writer.writerow([
+                student['id'],
+                student['group_name'],
+                student['name'],
+                student['integration_id'],
+                student['email'],
+                student['leader_id'],
+                student['leader_name'],
+                student['leader_email']
+            ])
 
 def main():
     print("Fetching groups...")
